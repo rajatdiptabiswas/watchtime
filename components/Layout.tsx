6d41d72c849c5d9b7686d3f15064ede6291823f2
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-08-14 18:21:27 +0600

    Add page title
    - Set page title to 'watchtime'

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react';
+import Head from 'next/head';
 import NavBar from './NavBar';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
+      <Head>
+        <title>watchtime</title>
+      </Head>
       <NavBar />
       <>{children}</>
     </>
