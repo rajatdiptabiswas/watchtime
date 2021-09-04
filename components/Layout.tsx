8b4ce8920c4a32b98ba2dbb393b2ce7dcfab16fe
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-09-04 13:24:16 +0600

    Add meta tags for SEO

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,6 +12,21 @@ export default function Layout({ children }: { children: ReactNode }) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+
+        {/* SEO */}
+        <meta name="title" content="watchtime" />
+        <meta
+          name="description"
+          content="Find what to watch based on the time available"
+        />
+        <meta
+          name="keywords"
+          content="watch, time, watchtime, tv, series, movie, tmdb, playback speed, search, recommendation, netflix, prime video, disney, hulu, apple tv, hbo max, paramount, peacock, youtube premium, showtime"
+        />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Rajat Dipta Biswas" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <NavBar />
       <>{children}</>
